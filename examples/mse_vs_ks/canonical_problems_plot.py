13977a5dd53d041a6e175ab60aa567aadae5bda7
Ney Secco <ney.rafa@gmail.com>
2022-02-26 20:21:04 -0300

    Added new type of plot for the MSE vs KS comparison

--- examples/mse_vs_ks/canonical_problems_plot.py
+++ examples/mse_vs_ks/canonical_problems_plot.py
@@ -9,6 +9,9 @@ import sys
 sys.path.append('../canonical_cases/ann_solution/')
 from test_cases_journal import load_case, plot_canonical
 
+# Figure properties
+exec(open("../figure_template.py").read())
+
 with open('./sweep_results.pickle','rb') as fid:
     results = pickle.load(fid)
 
@@ -24,8 +27,16 @@ print(valMSE_KS)
 # Get sizes
 num_cases, num_theta = valMSE_MSE.shape
 
-#===============================================
+# Define better names for each test case
+text_case_names = {'lin_adv':'Linear Advection',
+                   'lin_adv2':'Periodic Linear Advection',
+                   'heat_eq':'Steady Heat Transfer',
+                   'heat_cond':'Unsteady Heat Transfer',
+                   'burgers':"Burgers' Equation"}
+               
 
+#===============================================
+'''
 # Initialize figure
 
 fig = plt.figure()
@@ -206,6 +217,92 @@ for ii in range(num_cases):
     # Adjust splines
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
+'''
+#===============================================
+
+# Scatter + Histogram Plots
+
+# definitions for the axes
+left, width = 0.17, 0.6
+bottom, height = 0.13, 0.6
+spacing = 0.005
+
+rect_scatter = [left, bottom, width, height]
+rect_histx = [left, bottom + height + spacing, width, 0.2]
+rect_histy = [left + width + spacing, bottom, 0.2, height]
+
+# Auxiliary function to plot scatter and histograms
+def scatter_hist(valMSE_MSE, training_time_MSE,
+                 valMSE_KS, training_time_KS,
+                 case_name,
+                 ax, ax_histx, ax_histy):
+    # no labels
+    ax_histx.tick_params(axis="x", labelbottom=False)
+    ax_histy.tick_params(axis="y", labelleft=False)
+
+    # the scatter plot:
+    ax.plot(np.log10(valMSE_MSE), training_time_MSE, 'ok', label='MSE')
+    ax.plot(np.log10(valMSE_KS), training_time_KS, 'or', label='KS')
+
+    # now determine nice limits by hand:
+    MSE_min = min(np.min(valMSE_MSE), np.min(valMSE_KS))
+    MSE_max = max(np.max(valMSE_MSE), np.max(valMSE_KS))
+    MSE_min = 10**(np.floor(np.log10(MSE_min)))
+    MSE_max = 10**(np.ceil(np.log10(MSE_max)))
+
+    t_min = min(np.min(training_time_MSE), np.min(training_time_KS))
+    t_max = max(np.max(training_time_MSE), np.max(training_time_KS))
+
+    ax_histx.hist(np.log10(valMSE_MSE), 10, range=[np.log10(MSE_min), np.log10(MSE_max)], facecolor='k', alpha=0.75)
+    ax_histx.hist(np.log10(valMSE_KS), 10, range=[np.log10(MSE_min), np.log10(MSE_max)], facecolor='r', alpha=0.75)
+    ax_histy.hist(training_time_MSE, 10, range=[t_min, t_max], facecolor='k', alpha=0.75, orientation='horizontal')
+    ax_histy.hist(training_time_KS, 10, range=[t_min, t_max], facecolor='r', alpha=0.75, orientation='horizontal')
+
+    # Remove extra lines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel(r'$MSE_\textrm{val}$')
+    ax.set_ylabel(r'$T \; [s]$')
+
+    ax_histx.spines['top'].set_visible(False)
+    ax_histx.spines['right'].set_visible(False)
+    ax_histx.spines['left'].set_visible(False)
+    ax_histx.tick_params(axis='x', which=u'both', length=0, labelbottom=False)
+    ax_histx.tick_params(axis='y', which=u'both', length=0, labelleft=False)
+
+    ax_histy.spines['top'].set_visible(False)
+    ax_histy.spines['right'].set_visible(False)
+    ax_histy.spines['bottom'].set_visible(False)
+    ax_histy.tick_params(axis='x', which=u'both', length=0, labelbottom=False)
+    ax_histy.tick_params(axis='y', which=u'both', length=0, labelleft=False)
+
+    ax_histx.set_title(text_case_names[case_name])
+
+    # Set powers of 10 in the MSE axis
+    labels=ax.get_xticks().tolist()
+    labels = [r'$10^{%d}$'%item for item in labels]
+    ax.set_xticklabels(labels)
+
+    # Add legend only to the linear advection plot
+    if case_name == 'lin_adv':
+        ax.legend(loc='best')
+
+# Loop for every canonical case
+for ii in range(num_cases):
+
+    fig = plt.figure(figsize=(6.5, 7))
+
+    ax = fig.add_axes(rect_scatter)
+    ax_histx = fig.add_axes(rect_histx, sharex=ax)
+    ax_histy = fig.add_axes(rect_histy, sharey=ax)
+
+    # use the previously defined function
+    scatter_hist(valMSE_MSE[ii], training_time_MSE[ii],
+                 valMSE_KS[ii], training_time_KS[ii],
+                 canonical_prob[ii],
+                 ax, ax_histx, ax_histy)
+
+    fig.savefig('%s_scatter.pdf'%canonical_prob[ii])
 
 #===============================================
 
